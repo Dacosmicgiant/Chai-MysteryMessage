5a1c84ece4438219321c7e9517d0a95d6e5eed90
Dacosmicgiant <vedantvanpro@gmail.com>
2025-03-07 11:38:12 +0530

    React hook sign-up form and debouncing

---
+++ src/app/(auth)/verify/[username]/page.tsx
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/navigation'
+import React from 'react'
+
+function VerifyAccount() {
+    const router = useRouter()
+    const 
+  return (
+    <div>VerifyAccount</div>
+  )
+}
+
+export default VerifyAccount
